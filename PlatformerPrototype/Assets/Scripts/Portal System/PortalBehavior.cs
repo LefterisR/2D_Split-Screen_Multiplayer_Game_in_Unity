c9f4afddf594d90ed97c5e062df2add083fb8c59
LefterisR <162315856+LefterisR@users.noreply.github.com>
2024-08-11 20:59:26 +0300

    Added portal Animation Portal SFX and SFX pack to file

--- PlatformerPrototype/Assets/Scripts/Portal System/PortalBehavior.cs
+++ PlatformerPrototype/Assets/Scripts/Portal System/PortalBehavior.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using Unity.VisualScripting.Antlr3.Runtime;
+using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
 
+[RequireComponent(typeof(SpriteRenderer))]
 public class PortalBehavior : MonoBehaviour
 {
 
@@ -11,6 +13,57 @@ public class PortalBehavior : MonoBehaviour
 
     [SerializeField]
     private Transform dest;
+
+    private SpriteRenderer spriteRenderer;
+
+    public Sprite[] portalSprites;
+
+    private float animationTime = 0.2f;
+
+    private float animationTimerCnd = 0;
+
+    private int i = 0;
+
+    private AudioSource audioSource;
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        audioSource = GetComponent<AudioSource>();
+        animationTimerCnd = animationTime;
+    }
+
+
+    private void Update()
+    {
+        if (spriteRenderer != null) 
+        {
+
+            if (animationTimerCnd <= 0)
+            {
+
+                
+                if ((portalSprites.Length-1) == i)
+                {
+                    i = 0;
+                    spriteRenderer.sprite = portalSprites[i];
+                }
+                else
+                {
+                    i++;
+                    spriteRenderer.sprite = portalSprites[i];
+                }
+
+                Debug.Log(spriteRenderer.sprite.name);
+
+                animationTimerCnd = animationTime;
+            }
+            else 
+            {
+                animationTimerCnd-=Time.deltaTime;
+            }
+        
+        }
+    }
 
     //An object triggers the portal
     private void OnTriggerEnter2D(Collider2D collision)
@@ -22,7 +75,8 @@ public class PortalBehavior : MonoBehaviour
         }
 
         if (dest.TryGetComponent(out PortalBehavior endOfJourney)) 
-        {
+        {   
+            audioSource.Play();
             endOfJourney.portalAstonauts.Add(collision.gameObject); //Add the the hash set of the destination portal the traveller game object 
         }
 
@@ -33,5 +87,7 @@ public class PortalBehavior : MonoBehaviour
     {
         portalAstonauts.Remove(collision.gameObject); //Astonaut has left the starting portal
     }
+
+
 
 }
